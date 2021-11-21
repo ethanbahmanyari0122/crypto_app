cba4b8cfe82e434615b26b5fa402e6919cb3ca6b
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-21 17:40:09 +0100

    logo has been added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Button, Menu, Typography, Avatar} from "antd";
 import {Link } from "react-router-dom";
 import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from "@ant-design/icons";
+import icon from "../../images/crypto.png";
 
 const Navbar = () => {
     return(
