60af52d4ffc8b80d50b4aa981b6cafed143f8489
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-21 17:52:22 +0100

    css has been added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Switch , Route, Link} from 'react-router-dom'
 import {Layout, Typography, Space} from "antd";
-import Navbar from "./components/Navbar/Navbar";
+import {Navbar, Footer} from "./components";
 
 const App = ()=> {
     return(
