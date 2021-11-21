b4a191ce1bc1272cbf9674d55c58ab35d4c95761
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-21 18:26:23 +0100

    componenets has been added to the app.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export {default as Navbar} from "./Navbar/Navbar";
+export {default as Cryptocurrencies} from "./Cryptocurrencies/Cryptocurrencies";
+export {default as Cryptodetails} from "./Cryptodetails/Cryptodetails";
+export {default as Exchanges} from "./Exchanges/Exchanges";
+export {default as News} from "./News/News";
+export {default as Homepage} from "./Homepage/Homepage";
