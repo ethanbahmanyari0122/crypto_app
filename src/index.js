25dfdccf100b7a529d1ae1cf118412bdae0babed
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-21 18:33:30 +0100

    some css needed to be modified and antd css has been added to the server file index

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDom from "react-dom";
 
 import App from "./App";
 import {BrowserRouter as Router} from "react-router-dom";
+import 'antd/dist/antd.css';
 
 ReactDom.render(<Router><App/></Router>, document.getElementById('root'));
