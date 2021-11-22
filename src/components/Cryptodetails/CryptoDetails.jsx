ddc52610e057b198b09e999b2ef895283c63a94f
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 14:33:54 +0100

    crypto details page been made and api query is there

--- src/components/Cryptodetails/CryptoDetails.jsx
+++ src/components/Cryptodetails/CryptoDetails.jsx
@@ -12,6 +12,7 @@ const CryptoDetails = () => {
     const [timePeriod, setTimePeriod] = useState('7d')
     const {coinId} = useParams();
     const {data, isFetching} = useGetCryptoDetailsQuery(coinId)
+    const cryptoDetails = data?.data?.coin;
 
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
