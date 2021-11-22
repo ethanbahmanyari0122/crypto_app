9d0aaffe5f15bef4a9f089b6591a9b8d1401db9e
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 12:21:12 +0100

    working on cryptocurrencies page by fetching coins from the api

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
+import millify from "millify";
+import {Link} from 'react-router-dom';
+import {Card, Row, Col, Input} from "antd";
 
+
+import {useGetCryptosQuery} from "../../services/cryptoApi";
 
 const Cryptocurrencies = () => {
+    const {data: cryptoList, isFetching } = useGetCryptosQuery();
     return(
         <div>
 
