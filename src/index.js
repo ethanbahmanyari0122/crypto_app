1fba3152b6c2d2fce601052dee8678104c859992
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 11:42:13 +0100

    started implementing the coinranking api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
-
+import {Provider} from 'react-redux';
 import App from "./App";
 import {BrowserRouter as Router} from "react-router-dom";
 import 'antd/dist/antd.css';
+import store from './app/store'
 
-ReactDom.render(<Router><App/></Router>, document.getElementById('root'));
+ReactDom.render(<Router>
+    <Provider store={store}>
+        <App/>
+    </Provider>
+</Router>, document.getElementById('root'));
