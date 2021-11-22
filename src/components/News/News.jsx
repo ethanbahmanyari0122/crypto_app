f5a7fe46ed0ce3c319f6b86fd79330b15ccb8104
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 13:18:16 +0100

    news api has been added using redux toolkit

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -7,11 +7,23 @@ import {useGetCryptoNewsQuery} from "../../services/cryptoNews";
 const {Text, Title} = Typography;
 const { Option } = Select;
 const News = ({simplified}) => {
-    const {data: cryptoNews} = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 12 : 100})
-    return(
-        <div>
+    const {data: cryptoNews} = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12})
 
-        </div>
+    if(!cryptoNews?.value) return "Loading..."
+    return(
+        <Row gutter={[24,24]}>
+            {cryptoNews.value.map((news, i) => (
+                <Col xs={24} sm={12} lg={8} key={i}>
+                    <Card hoverable className="news-card">
+                        <a href={news.url} target="_blank" rel="noreferrer">
+                            <div className="news-image-container">
+                                <Title className="news-title" level={4}>{news.name}</Title>
+                            </div>
+                        </a>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
     )
 }
 
