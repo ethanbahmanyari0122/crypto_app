4a6fda3be71c37714fb6d72f2b1aa1cea32abcaf
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 12:17:50 +0100

    working on homepage by fetching coins from the api

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -3,6 +3,8 @@ import millify from "millify";
 import  {Typography, Row, Col, Statistic} from "antd";
 import {Link} from "react-router-dom";
 import { useGetCryptosQuery} from "../../services/cryptoApi";
+import Cryptocurrencies from "../Cryptocurrencies/Cryptocurrencies";
+import News from "../News/News";
 
 const {Title} = Typography;
 
@@ -24,8 +26,13 @@ const Homepage = () => {
             <div className="home-heading-container">
                 <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
                 <Title level={2} className="show-more"><Link to='/cryptocurrencies'>Show More</Link></Title>
-
             </div>
+            <Cryptocurrencies simplified/>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={2} className="show-more"><Link to='/news'>Show More</Link></Title>
+            </div>
+            <News simplified/>
         </>
     )
 }
