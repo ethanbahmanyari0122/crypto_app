8c54c9c31eabad2f88d4ad7698cd3cf62cdb66cd
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 13:09:27 +0100

    news api has been added using redux toolkit

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import  {Select, Typography, Row, Col, Avatar, Card} from "antd";
 import moment from "moment";
+import {useGetCryptoNewsQuery} from "../../services/cryptoNews";
 
-const News = () => {
+
+const {Text, Title} = Typography;
+const { Option } = Select;
+const News = ({simplified}) => {
+    const {data: cryptoNews} = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 12 : 100})
     return(
         <div>
 
