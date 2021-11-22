b975a3f96c09596979037b825f36b66b3e8e6b13
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 11:38:10 +0100

    started implementing the coinranking api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,9 @@
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//         'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//         'x-rapidapi-key': '472f58a7edmsha4265f5d786161dp1b5e3djsn89d3add5c915'
+//     }
+// };
