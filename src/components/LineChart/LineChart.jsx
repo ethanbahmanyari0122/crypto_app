aa4611f5e6bddc517d96a76779033294e77c16b7
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 15:32:58 +0100

    chart component been made

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -6,9 +6,21 @@ const {Title} = Typography;
 
 const LineChart = ({coinHistory, currentPrice, coinName}) => {
   return(
-      <div>
-
-      </div>
+      <>
+        <Row className="chart-header">
+            <Title level={2} className="chart-title">
+                {coinName} Price Chart
+            </Title>
+            <Col className="price-container">
+                <Title level={5} className="price-change">
+                    {coinHistory?.data?.change}%
+                </Title>
+                <Title level={5} className="current-price">
+                    Current {coinName} Price: $ {currentPrice}
+                </Title>
+            </Col>
+        </Row>
+      </>
   )
 }
 
