e3cc25cfe21214452d46b29a7de6f8899a36f0a9
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 13:12:31 +0100

    news api has been added using redux toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 
 import { cryptoApi} from "../services/cryptoApi";
+import { cryptoNews } from "../services/cryptoNews";
 
 export default configureStore({
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNews.reducerPath]: cryptoNews.reducer,
     },
 });
