977180875a9fb9760d9e9babc40295ec8995fcf8
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 15:13:40 +0100

    crypto details page been made and api query is there

--- src/components/Cryptodetails/CryptoDetails.jsx
+++ src/components/Cryptodetails/CryptoDetails.jsx
@@ -93,6 +93,28 @@ const CryptoDetails = () => {
                     ))}
                 </Col>
             </Col>
+            <Col className="coin-desc-link">
+                <Row className="coin-desc">
+                    <Title level={3} className="coin-details-heading">
+                        What is {cryptoDetails.name}
+                        {HTMLReactParser(cryptoDetails.description)}
+                    </Title>
+                </Row>
+                <Col className="coin-links">
+                    <Title level={3} className="coin-details-heading">
+                        {cryptoDetails.name} Links
+                    </Title>
+                    {cryptoDetails.links.map((link) =>
+                    <Row className="coin-link" key={link.name}>
+                        <Title level={5} className="link-name">
+                            {link.type}
+                        </Title>
+                        <a href={link.url} target="_blank" rel="noreferrer">
+                            {link.name}
+                        </a>
+                    </Row>)}
+                </Col>
+            </Col>
         </Col>
     )
 }
