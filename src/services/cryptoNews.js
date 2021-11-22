a0b65125e85d5ceaa283e744d8d5179cf9a35261
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 13:04:59 +0100

    news api has been added using redux toolkit

---
+++ src/services/cryptoNews.js
@@ -0,0 +1,25 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '472f58a7edmsha4265f5d786161dp1b5e3djsn89d3add5c915'
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers:cryptoNewsHeaders })
+
+
+export const cryptoNews = createApi({
+    reducerPath: 'cryptoNews',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) =>({
+        getCryptoNews: builder.query({
+            query: ({newsCategory, count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+});
+
+export const {useGetCryptosQuery} = cryptoApi;
