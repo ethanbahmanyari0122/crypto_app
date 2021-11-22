2b0aa2740f1adb274cecf9fb7f7103adac6952d5
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 13:39:37 +0100

    news api has been added using redux toolkit

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -14,13 +14,26 @@ const News = ({simplified}) => {
     if(!cryptoNews?.value) return "Loading..."
     return(
         <Row gutter={[24,24]}>
+            {!simplified && (
+                <Col span={24}>
+                    <Select showSearch
+                            className="select-news"
+                            placeholder="Select a Crypto"
+                            optionFilterProp="children"
+                            onChange={(value => )}
+                            filterOption={(input, optiom)=> option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                    >
+
+                    </Select>
+                </Col>
+            )}
             {cryptoNews.value.map((news, i) => (
                 <Col xs={24} sm={12} lg={8} key={i}>
                     <Card hoverable className="news-card">
                         <a href={news.url} target="_blank" rel="noreferrer">
                             <div className="news-image-container">
                                 <Title className="news-title" level={4}>{news.name}</Title>
-                                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news"/>
+                                <img style={{maxWidth: '200px', maxHeight: '100px'}} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news"/>
                             </div>
                             <p>
                                 {news.description > 100 ? `${news.description.substring(0, 100)}...`
@@ -29,9 +42,9 @@ const News = ({simplified}) => {
                             <div className="provider-container">
                                 <div>
                                     <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} />
-                                    <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                                    <Text className="provider-name">{news.provider[0]?.name}</Text>
                                 </div>
-
+                                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
                             </div>
                         </a>
                     </Card>
