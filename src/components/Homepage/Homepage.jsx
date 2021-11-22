87106b6261b0c415a1446a1e5cbb8bd4ab7d269c
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 12:12:57 +0100

    working on homepage by fetching coins from the api

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -16,10 +16,10 @@ const Homepage = () => {
             <Title level={2} className="heading">Global Crypto Stats</Title>
             <Row>
                 <Col span={12}><Statistic title="Total Crypto Currencies" value={globalStats.total}/></Col>
-                <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
-                <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
-                <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
-                <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
             </Row>
         </>
     )
