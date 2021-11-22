1db16c5164caeab10904dd920df0e5717106ade0
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 15:19:22 +0100

    chart component been made

--- src/components/Cryptodetails/CryptoDetails.jsx
+++ src/components/Cryptodetails/CryptoDetails.jsx
@@ -5,6 +5,7 @@ import millify from "millify";
 import {Col, Row, Typography, Select} from "antd";
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 import {useGetCryptoDetailsQuery} from "../../services/cryptoApi"
+import LineChart from "../LineChart/LineChart"
 
 const {Title, Text} = Typography;
 const {Option} = Select;
@@ -53,6 +54,7 @@ const CryptoDetails = () => {
                     >
                 {time.map((date)=> <option key={date}>{date}</option>)}
             </Select>
+            <LineChart coinHistory={coinHistory} currentPrice = {millify(cryptoDetails.price)} coinName={cryptoDetails.name}/>
             <Col className="stats-container">
                 <Col className="coin-value-statistics">
                     <Col className="coin-value-statistics-heading">
--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import {Line} from 'react-chartjs-2';
+import {Col, Row, Typography} from "antd";
 
+const
 
 const LineChart = () => {
   return(
