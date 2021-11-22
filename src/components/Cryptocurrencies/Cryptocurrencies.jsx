86a4dd643e62f7a0aa1896cf9f3748c8dba2fd97
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 12:36:41 +0100

    working on cryptocurrencies page by fetching coins from the api

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -6,14 +6,16 @@ import {Card, Row, Col, Input} from "antd";
 
 import {useGetCryptosQuery} from "../../services/cryptoApi";
 
-const Cryptocurrencies = () => {
-    const {data: cryptosList, isFetching } = useGetCryptosQuery();
+const Cryptocurrencies = ({simplified}) => {
+    const count = simplified ? 10 : 100;
+    const {data: cryptosList, isFetching } = useGetCryptosQuery(count);
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
 
+    if(isFetching) return 'Loading...'
     return(
         <>
             <Row gutter={[32,32]} className="crypto-card-container">
-                {cryptos.map((currency)=>(
+                {cryptos?.map((currency)=>(
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
                         <Link to={`/crypto/${currency.id}`}>
                             <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl}/>} hoverable>
