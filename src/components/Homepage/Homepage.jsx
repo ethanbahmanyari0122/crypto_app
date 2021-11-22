dcdfe9e63dc632011769a6a067bb40543f3cd682
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 11:31:32 +0100

    home page started

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import millify from "millify";
+import  {Typography, Row, Col, Statistic} from "antd";
+import {Link} from "react-router-dom";
 
+const {Title} = Typography;
 
 const Homepage = () => {
     return(
-        <div>
-
-        </div>
+        <>
+            <Title level={2} className="heading">Global Crypto Stats</Title>
+        </>
     )
 }
 
