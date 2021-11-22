b5a7d12da6a42c6ca1080dd1a894501477bacc7b
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 11:54:00 +0100

    started implementing the coinranking api

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import millify from "millify";
 import  {Typography, Row, Col, Statistic} from "antd";
 import {Link} from "react-router-dom";
+import { useGetCryptosQuery} from "../../services/cryptoApi";
 
 const {Title} = Typography;
 
 const Homepage = () => {
+    const { data, isFetching} = useGetCryptosQuery();
+
     return(
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
