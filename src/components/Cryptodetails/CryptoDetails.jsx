fafe88824fe42453e7c2e1745f2a606744c39e6e
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 15:03:26 +0100

    crypto details page been made and api query is there

--- src/components/Cryptodetails/CryptoDetails.jsx
+++ src/components/Cryptodetails/CryptoDetails.jsx
@@ -73,6 +73,22 @@ const CryptoDetails = () => {
                         </Col>
                     ))}
                 </Col>
+                <Col className="other-stats-info">
+                    <Col className="coin-value-statistics-heading">
+                        <Title level={3} className="coin-details-heading">
+                            Other Statistics
+                        </Title>
+                    </Col>
+                    {genericStats.map(({icon, title,value}) => (
+                        <Col className="coin-stats">
+                            <Col className="coin-stats-name">
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className="stats">{value}</Text>
+                        </Col>
+                    ))}
+                </Col>
             </Col>
         </Col>
     )
