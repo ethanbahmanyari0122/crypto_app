bcaaa0c36adabb11cf0e03e365b461302fdfc608
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 14:27:31 +0100

    crypto details page been made and api query is there

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -15,7 +15,10 @@ export const cryptoApi = createApi({
     endpoints: (builder) =>({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
-        })
+        }),
+        getCryptoDetails: (builder.query({
+            query: (coinId) => createRequest(`/coins/${coinId}`),
+        }))
     })
 });
 
