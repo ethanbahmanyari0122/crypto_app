654f0bfca8c6d86a0e9c1b3b870daeddc22baa9b
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 11:33:51 +0100

    home page started

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -9,6 +9,13 @@ const Homepage = () => {
     return(
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Crypto Currencies" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
+            </Row>
         </>
     )
 }
