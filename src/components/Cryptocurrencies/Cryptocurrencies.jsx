2630dd15f15e4bd1567868e0c148325a258e904a
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 12:51:42 +0100

    search bar has been added to the cryptocurrencies page

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -7,7 +7,7 @@ import {Card, Row, Col, Input} from "antd";
 import {useGetCryptosQuery} from "../../services/cryptoApi";
 
 const Cryptocurrencies = ({simplified}) => {
-    const count = simplified ? 10 : 100;
+    const count = simplified ? 12 : 100;
     const {data: cryptosList, isFetching } = useGetCryptosQuery(count);
     const [cryptos, setCryptos] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
@@ -20,9 +20,11 @@ const Cryptocurrencies = ({simplified}) => {
     if(isFetching) return 'Loading...'
     return(
         <>
+            {!simplified && (
             <div className="search-crypto">
                 <Input placeholder="Search Crypto" onChange={(e)=> setSearchTerm(e.target.value)}/>
             </div>
+            )}
             <Row gutter={[32,32]} className="crypto-card-container">
                 {cryptos?.map((currency)=>(
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
