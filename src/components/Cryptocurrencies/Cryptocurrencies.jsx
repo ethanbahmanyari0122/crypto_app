8a4c850101eb18e21830e74e95540ab4ad14fd7f
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-11-22 12:44:55 +0100

    search bar has been added to the cryptocurrencies page

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState , useEffect} from "react";
 import millify from "millify";
 import {Link} from 'react-router-dom';
 import {Card, Row, Col, Input} from "antd";
@@ -9,8 +9,13 @@ import {useGetCryptosQuery} from "../../services/cryptoApi";
 const Cryptocurrencies = ({simplified}) => {
     const count = simplified ? 10 : 100;
     const {data: cryptosList, isFetching } = useGetCryptosQuery(count);
-    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+    const [cryptos, setCryptos] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(()=>{
+        const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        setCryptos(filteredData);
+    },[cryptosList, searchTerm])
 
     if(isFetching) return 'Loading...'
     return(
